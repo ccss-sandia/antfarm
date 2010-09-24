da192464cb8e9127a3f1e8b02beac4414a922ea1
Bryan T. Richardson <scada@sandia.gov>
2010-09-24 15:06:16 -0600

    more OCD stuff... being consistent with code style

--- lib/antfarm/cli/plugin.rb
+++ lib/antfarm/cli/plugin.rb
@@ -8,19 +8,23 @@ module Antfarm
       def self.show_info(plugin)
         table        = Antfarm::CLI::UI::Console::Table.new
         table.header = ['Plugin Info', '']
+
         for key in ALLOWED_INFO
           table.add_row([key.to_s.capitalize, plugin.info[key].to_s])
         end
+
         table.print
       end
 
       def self.show_options(plugin)
         table        = Antfarm::CLI::UI::Console::Table.new
         table.header = DISPLAYED_OPTIONS.map { |key| key.to_s.capitalize }
+
         for option in plugin.options
           row = DISPLAYED_OPTIONS.map { |key| key == :name ? "--#{option[key].to_s.gsub(/_/,'-')}" : option[key].to_s }
           table.add_row(row)
         end
+
         puts 'Plugin Options:'
         table.print
       end
