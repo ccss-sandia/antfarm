0ce3df8815ab41232eb7f069c118d095338bb83c
Bryan T. Richardson <btricha@sandia.gov>
2013-03-13 13:39:44 -0600

    Rearranging some code to get existing tests to pass

--- lib/antfarm/helpers.rb
+++ lib/antfarm/helpers.rb
@@ -32,54 +32,55 @@
 require 'fileutils'
 
 module Antfarm
+  # Symbolic marker points on the fuzzy logic certainty factor scale.
+  # Certainty Factors (CF)
+  CF_PROVEN_TRUE   =  1.0000
+  CF_LIKELY_TRUE   =  0.5000
+  CF_LACK_OF_PROOF =  0.0000
+  CF_LIKELY_FALSE  = -0.5000
+  CF_PROVEN_FALSE  = -1.0000
+
+  # Amount by which a value can differ and still be considered the same.
+  # Mainly used as a buffer against floating point round-off errors.
+  CF_VARIANCE      =  0.0001
+
+  @user_dir        = nil
+  @outputter       = nil
+  @logger_callback = nil
+
+  class << self
+    attr_accessor :logger_callback
+    attr_accessor :outputter
+    attr_accessor :user_dir
+  end
+
+  def self.clamp(x, low = CF_PROVEN_FALSE, high = CF_PROVEN_TRUE)
+    if x < low
+      return low
+    elsif x > high
+      return high
+    else
+      return x
+    end
+  end
+
+  def self.output(message)
+    @outputter.puts(message) unless @outputter.nil?
+  end
+
+  def self.log(level, *msg)
+    @logger_callback.call(level, msg.join) if @logger_callback
+  end
+
+  def self.simplify_interfaces
+    #TODO
+  end
+
+  def self.timestamp
+    return Time.now.utc.xmlschema
+  end
+
   module Helpers
-    # Symbolic marker points on the fuzzy logic certainty factor scale.
-    # Certainty Factors (CF)
-    CF_PROVEN_TRUE   =  1.0000
-    CF_LIKELY_TRUE   =  0.5000
-    CF_LACK_OF_PROOF =  0.0000
-    CF_LIKELY_FALSE  = -0.5000
-    CF_PROVEN_FALSE  = -1.0000
-
-    # Amount by which a value can differ and still be considered the same.
-    # Mainly used as a buffer against floating point round-off errors.
-    CF_VARIANCE      =  0.0001
-
-    @user_dir        = nil
-    @outputter       = nil
-    @logger_callback = nil
-    class << self
-      attr_accessor :logger_callback
-      attr_accessor :outputter
-      attr_accessor :user_dir
-    end
-
-    def self.clamp(x, low = CF_PROVEN_FALSE, high = CF_PROVEN_TRUE)
-      if x < low
-        return low
-      elsif x > high
-        return high
-      else
-        return x
-      end
-    end
-
-    def self.simplify_interfaces
-      #TODO
-    end
-
-    def self.timestamp
-      return Time.now.utc.xmlschema
-    end
-
-    def self.output(message)
-      @outputter.puts(message) unless @outputter.nil?
-    end
-
-    def self.log(level, *msg)
-      @logger_callback.call(level, msg.join) if @logger_callback
-    end
-
     def self.db_dir
       return File.expand_path("#{self.user_dir}/db")
     end
