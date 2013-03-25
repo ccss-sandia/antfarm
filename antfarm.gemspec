beedbe5aa1de256a013b447e659e1694214389b2
Bryan T. Richardson <btricha@sandia.gov>
2013-03-25 11:04:49 -0600

    Initial commit of PCAP parser plugin
    Using PacketFu this time around to parse PCAP data. My hope is that by
    using PacketFu, we can create custom protocol representations in the
    PacketFu library to determine if a packet is for a particular protocol
    of interest (i.e. create PacketFu protocol parsers for each control
    system protocol we're interested in).
    
    The other option for detecting protocols is to develop a filter
    capability that packet parsers run packets through each time they parse
    traffic.
    
    Right now, this plugin is looking at the MAC addresses and creating an
    ethernet interface and associated IP interface if the MAC address
    isn't found. Need to improve on this by first checking to see if the IP
    interface already exists, and if so updating it to include the MAC
    address defined in the packet. There may be cases in which the IP
    interface already has an Ethernet interface. If it does and the MAC
    address doesn't match the one on the wire, we'll need to updated based
    on certainty factors.

--- antfarm.gemspec
+++ antfarm.gemspec
@@ -50,6 +50,7 @@ Gem::Specification.new do |s|
   s.has_rdoc      = false
 
   s.add_dependency 'activerecord', '= 3.2.12'
+  s.add_dependency 'packetfu',     '= 1.1.6'
   s.add_dependency 'pg',           '= 0.14.1'
   s.add_dependency 'sqlite3',      '= 1.3.7'
   s.add_dependency 'trollop',      '= 2.0'
---
+++ lib/antfarm/plugins/pcap.rb
@@ -0,0 +1,80 @@
+module Antfarm
+  module Pcap
+    def self.registered(plugin)
+      plugin.name = 'pcap'
+      plugin.info = {
+        :desc   => 'Parse PCAP data',
+        :author => 'Bryan T. Richardson'
+      }
+      plugin.options = [{
+        :name     => 'file',
+        :desc     => 'File containing PCAP data',
+        :type     => String,
+        :required => true
+      }]
+    end
+
+    def run(opts = Hash.new)
+      check_options(opts)
+
+      require 'packetfu'
+
+      if File.readable?(opts[:file])
+        PacketFu::PcapFile.read_packets(opts[:file]) do |pkt|
+          if pkt.proto.include?('IP')
+            smaddr = pkt.eth_saddr.upcase
+            dmaddr = pkt.eth_daddr.upcase
+            siaddr = pkt.ip_saddr
+            diaddr = pkt.ip_daddr
+
+            if s_eth_iface = Antfarm::Models::EthernetInterface.find_by_address(smaddr)
+              next
+            else
+              node    = Antfarm::Models::Node.create! :certainty_factor => 0.5, :device_type => 'PCAP'
+              l2iface = node.layer2_interfaces.create! :certainty_factor => 0.75,
+                          :ethernet_interface_attributes => { :address => smaddr }
+
+              l2iface.tags.create! :name => Antfarm::OuiParser.get_name(smaddr) || 'Unknown Vendor'
+
+              l3iface = l2iface.layer3_interfaces.create! :certainty_factor => 0.75,
+                          :ip_interface_attributes => { :address => siaddr }
+
+              if pkt.proto.include?('TCP')
+                if pkt.tcp_src == 502
+                  l3iface.tags.create! :name => 'Modbus TCP Slave'
+                elsif pkt.tcp_dst == 502
+                  l3iface.tags.create! :name => 'Modbus TCP Master'
+                end
+              end
+            end
+
+            if d_eth_iface = Antfarm::Models::EthernetInterface.find_by_address(dmaddr)
+              next
+            else
+              node    = Antfarm::Models::Node.create! :certainty_factor => 0.5, :device_type => 'PCAP'
+              l2iface = node.layer2_interfaces.create! :certainty_factor => 0.75,
+                          :ethernet_interface_attributes => { :address => dmaddr }
+
+              l2iface.tags.create! :name => Antfarm::OuiParser.get_name(dmaddr) || 'Unknown Vendor'
+
+              l3iface = l2iface.layer3_interfaces.create! :certainty_factor => 0.75,
+                          :ip_interface_attributes => { :address => diaddr }
+
+              if pkt.proto.include?('TCP')
+                if pkt.tcp_src == 502
+                  l3iface.tags.create! :name => 'Modbus TCP Master'
+                elsif pkt.tcp_dst == 502
+                  l3iface.tags.create! :name => 'Modbus TCP Slave'
+                end
+              end
+            end
+          end
+        end
+      else
+        raise "Infput file #{opts[:file]} doesn't exist."
+      end
+    end
+  end
+end
+
+Antfarm.register(Antfarm::Pcap)
