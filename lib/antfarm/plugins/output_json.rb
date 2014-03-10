fd7c34df362120ac06e2a5ff95c765994de7eb02
Bryan T. Richardson <btricha@sandia.gov>
2014-03-10 11:04:47 -0600

    New option for specifying type of devices to include
    Users can now specify multiple types of devices to include in the JSON
    output based on the `type` attribute for node objects.

--- lib/antfarm/plugins/output_json.rb
+++ lib/antfarm/plugins/output_json.rb
@@ -13,6 +13,12 @@ module Antfarm
         :required => true
       },
       {
+        :name => 'device_types',
+        :desc => 'Device types (separated by commas) to include',
+        :type     => String,
+        :required => true
+      },
+      {
         :name => 'include_nodes',
         :desc => 'Include normal nodes in output'
       }]
@@ -26,20 +32,35 @@ module Antfarm
       nodes = Array.new
       links = Array.new
 
-      data = { :nodes => nodes, :links => links }
+      data  = { :nodes => nodes, :links => links }
+      types = opts[:device_types].split(',')
 
       node_indexes = Hash.new
       net_indexes  = Hash.new
 
       Antfarm::Models::Layer3Network.all.each do |network|
-        net_indexes[network.id] = nodes.length
-        nodes << { :name => network.id, :group => 'LAN', :label => network.ip_network.address }
+        if opts[:include_nodes]
+          display = true
+        else
+          display = false
+          network.layer3_interfaces.each do |iface|
+            if types.include?(iface.layer2_interface.node.device_type)
+              display = true
+              break
+            end
+          end
+        end
+
+        if display
+          net_indexes[network.id] = nodes.length
+          nodes << { :name => "net-#{network.id}", :group => 'LAN', :label => network.ip_network.address }
+        end
       end
 
       Antfarm::Models::Node.all.each do |node|
-        if opts[:include_nodes] or node.device_type == 'Cisco PIX/ASA'
+        if opts[:include_nodes] or types.include?(node.device_type)
           node_indexes[node.id] = nodes.length
-          nodes << { :name => node.id, :group => node.device_type, :label => node.name }
+          nodes << { :name => "node-#{node.id}", :group => node.device_type, :label => node.name }
 
           node.layer3_interfaces.each do |iface|
             links << { :source => node_indexes[node.id], :target => net_indexes[iface.layer3_network.id], :value => 1 }
