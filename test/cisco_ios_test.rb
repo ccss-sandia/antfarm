9e9b37f94a8eb019f7709ef9d52e30e812a6cabe
Bryan T. Richardson <btricha@sandia.gov>
2014-03-21 20:21:14 -0600

    Moved `read_file` override in Cisco IOS test to be inside block
    This way, when running with other plugin tests, the overrides don't
    fight each other and cause tests to fail.

--- test/cisco_ios_test.rb
+++ test/cisco_ios_test.rb
@@ -1,31 +1,32 @@
 require 'test_helper'
 
-class Antfarm::Plugin
-  def read_data(path, &block)
-    data = StringIO.new
-
-    data << "version 12.2\n"
-    data << "hostname InetRouter\n"
-    data << "network-object 192.168.0.0 255.255.255.0\n"
-    data << "network-object host 192.168.0.101\n"
-    data << "interface FastEthernet0/0\n"
-    data << " ip address 10.0.1.1 255.255.255.0\n"
-    data << " duplex full\n"
-    data << " speed auto\n"
-    data << "interface FastEthernet0/1\n"
-    data << " ip address 10.0.2.254 255.255.255.0\n"
-    data << " duplex full\n"
-    data << " speed auto\n"
-    data << "ip route 10.0.3.0 255.255.255.0 10.0.2.1\n"
-
-    data.rewind
-
-    yield path, data
-  end
-end
-
 class CiscoIOSTest < TestCase
   test 'Cisco IOS config parsing' do
+
+    class Antfarm::Plugin
+      def read_data(path, &block)
+        data = StringIO.new
+
+        data << "version 12.2\n"
+        data << "hostname InetRouter\n"
+        data << "network-object 192.168.0.0 255.255.255.0\n"
+        data << "network-object host 192.168.0.101\n"
+        data << "interface FastEthernet0/0\n"
+        data << " ip address 10.0.1.1 255.255.255.0\n"
+        data << " duplex full\n"
+        data << " speed auto\n"
+        data << "interface FastEthernet0/1\n"
+        data << " ip address 10.0.2.254 255.255.255.0\n"
+        data << " duplex full\n"
+        data << " speed auto\n"
+        data << "ip route 10.0.3.0 255.255.255.0 10.0.2.1\n"
+
+        data.rewind
+
+        yield path, data
+      end
+    end
+
     opts = { :file => '/foo/bar/sucka' }
     Antfarm.plugins['cisco-ios'].run(opts)
 
